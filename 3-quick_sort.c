131f16c568014edcaac79b2e046556096811faf7
juandraper <e146613@gmail.com>
2021-10-25 15:20:33 -0700

    fixing

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -8,15 +8,15 @@
  */
 void swinger(int *left, int *right)
 {
-        int t;
+	int t;
 
-        t = *left;
-        *left = *right;
-        *right = t;
+	t = *left;
+	*left = *right;
+	*right = t;
 }
 
 /**
- * partScheme - Lomuto's partition 
+ * partScheme - Lomuto's partition
  * @array: Array to be parted
  * @size: size of the whole array
  * @left: start index
@@ -25,25 +25,25 @@ void swinger(int *left, int *right)
  */
 int partScheme(int *array, int size, int left, int right)
 {
-        int p, pi, j;
+	int p, pi, j;
 
-        p = array[right];
-        pi = left;
+	p = array[right];
+	pi = left;
 
-        for (j = left; j < right; j++)
-        {
-                if (array[j] <= p)
-                {
-                        swinger(&array[pi], &array[j]);
-                        if (pi != j)
-                                print_array(array, size);
-                        pi++;
-                }
-        }
-        swinger(&array[pi], &array[right]);
-        if (pi != j)
-                print_array(array, size);
-        return (pi);
+	for (j = left; j < right; j++)
+	{
+		if (array[j] <= p)
+		{
+			swinger(&array[pi], &array[j]);
+			if (pi != j)
+				print_array(array, size);
+			pi++;
+		}
+	}
+	swinger(&array[pi], &array[right]);
+	if (pi != j)
+		print_array(array, size);
+	return (pi);
 }
 
 /**
@@ -56,14 +56,14 @@ int partScheme(int *array, int size, int left, int right)
  */
 void lqs(int *array, int size, int left, int right)
 {
-        int p;
+	int p;
 
-        if (left < right)
-        {
-                p = partScheme(array, size, left, right);
-                lqs(array, size, left, p - 1);
-                lqs(array, size, p + 1, right);
-        }
+	if (left < right)
+	{
+		p = partScheme(array, size, left, right);
+		lqs(array, size, left, p - 1);
+		lqs(array, size, p + 1, right);
+	}
 
 }
 
@@ -75,6 +75,6 @@ void lqs(int *array, int size, int left, int right)
  */
 void quick_sort(int *array, size_t size)
 {
-        lqs(array, size, 0, size - 1);
+	lqs(array, size, 0, size - 1);
 }
 
