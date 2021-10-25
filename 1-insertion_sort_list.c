47be305517e49f46b254507bed1f907bb9232744
root <3290@holbertonschool.com>
2021-10-25 11:41:05 -0700

    insertion 1 fin

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,36 +1,39 @@
 #include "sort.h"
 /**
- *
+ * insertion_sort_list - Function sorted the doubly linked list in ascending
+ * @list: list node
+ * Return: Success Always 0
  */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *i;
-	listint_t *f;
+	listint_t *i, *f;
 
 	if (!list || !(*list))
 		return;
 	i = *list;
+
 	while (i)
 	{
 		f = i;
-		while (f->prev || f->n < f->prev->n)
+		while (f->prev && f->n < f->prev->n)
 		{
 			f->prev->next = f->next;
+
 			if (f->next)
 			{
 				f->next->prev = f->prev;
 			}
 
-		f->next = f->prev;
-		f->prev = f->prev->prev;
-		f->prev->next = f;
-		if (!f->prev)
-			*list = f;
-		else
-		
-			f->prev->next = f;
-		print_list(*list);
+			f->next = f->prev;
+			f->prev = f->prev->prev;
+			f->next->prev = f;
+
+			if (!f->prev)
+				*list = f;
+			else
+				f->prev->next = f;
+			print_list(*list);
 		}
+		i = i->next;
 	}
-	i = i->next;
 }
